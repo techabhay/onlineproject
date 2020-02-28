e834327090553a863cccae13a62f165462df4839
ABHAY <abhaymishra008@gmail.com>
2020-02-28 19:39:46 +0530

    DemoProject

---
+++ demo/src/main/java/com/abhay/demo/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.abhay.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abhay.demo.dao.EmployeeRepository;
+import com.abhay.demo.entity.Employee;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	@GetMapping("/all")
+	public List<Employee> getAllEmployees() {		
+		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("eid/{id}")
+	public Employee getEmployeeById(@PathVariable("id")Integer id) {
+		return employeeRepository.findById(id).orElse(new Employee());
+	}
+	
+	@PostMapping("/save")
+	public Employee saveEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+}
---
+++ demo/src/main/java/com/abhay/demo/entity/Employee.java
@@ -0,0 +1,41 @@
+package com.abhay.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int eid;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String salary;
+
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}	
+	
+}
